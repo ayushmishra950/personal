9d180e2f04b8a8442ace1f2dea124eff1ba08137
ayushmishra950 <ayush950mishra@gmail.com>
2025-08-28 20:30:13 +0530

    Update Login.js

--- social_media/src/components/login/Login.js
+++ social_media/src/components/login/Login.js
@@ -81,7 +81,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        'http://localhost:5000/graphql',
+        'https://personal-ewn7.onrender.com/graphql',
         { query, variables },
         {
           headers: {
